0dfdc3dd19484625baf85816243e98f4f31a40ba
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-05-03 22:07:33 -0300

    pagina do diretor adicionada

---
+++ src/pages/Board/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Board = () => {
+  return (
+    <div>
+      <div>Ola </div>
+      <div>Bem vindo</div>
+      <div>diretor!</div>
+    </div>
+  );
+};
--- src/template/index.jsx
+++ src/template/index.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import { Class } from "../pages/Class";
 import { ContextValues } from "../context/ContextValues";
+import { Board } from "../pages/Board";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" exact Component={Home} />
           <Route path="/turma" exact Component={Class} />
+          <Route path="/diretoria" exact Component={Board} />
         </Routes>
       </ContextValues>
     </Router>
